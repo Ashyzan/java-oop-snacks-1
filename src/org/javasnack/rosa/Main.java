19f96b96df0e426340adf33a1774137031227be1
RosaCielo <ashyzan@gmail.com>
2024-05-21 12:25:00 +0200

    ADD 2 classes
    main and registro studenti

---
+++ src/org/javasnack/rosa/Main.java
@@ -0,0 +1,15 @@
+package org.javasnack.rosa;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		RegistroStudenti registo1 = new RegistroStudenti();
+		
+		registo1.aggiungiStudente({pippo, luisa, gino});
+		
+		registo1.stampaListaStudenti();
+		
+	}
+
+}
--- src/org/javasnack/rosa/RegistroStudenti.java
+++ src/org/javasnack/rosa/RegistroStudenti.java
@@ -1,5 +1,55 @@
 package org.javasnack.rosa;
 
-public class RegistroStudenti {
+import java.sql.Array;
+import java.util.Arrays;
 
+public class RegistroStudenti {
+	
+	private String [] arrayStudente;
+	
+	
+	
+	
+	// costruttore per inizializzare il registro vuoto
+	
+	public RegistroStudenti() {
+		
+		this.arrayStudente = arrayStudente;
+	}
+	
+	
+	// getter e setter
+	
+	
+	public String[] getArrayStudente() {
+		return arrayStudente;
+	}
+
+
+	public void setArrayStudente(String[] arrayStudente) {
+		this.arrayStudente = arrayStudente;
+	}
+	
+	
+	// metodo pubblico per aggiungere studenti al registro.
+
+	public void aggiungiStudente(String [] studenti) {
+		
+		
+		setArrayStudente(studenti);
+		
+				
+	}
+	
+	//Aggiungi un metodo che stampi la lista degli studenti.
+	
+	public void stampaListaStudenti() {
+		
+		System.out.println(getArrayStudente()); 
+		 
+	}
+	
+
+	  
+ 
 }
